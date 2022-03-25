359625b4e48e5b4d71ddbbb8bb48056573445278
Roy <roy.rosario88@gmail.com>
2022-03-25 11:02:46 -0400

    added a hero image

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import './App.css'
-import {AppBar, Toolbar, Typography } from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Box from '@material-ui/core/Box'
 
-
+const useStyles = makeStyles((theme) =>({
+  appBar:{
+    backgroundColor: "#fff"
+  },
+  hero:{
+    backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url("https://images.unsplash.com/photo-1533074470141-d34a0d5212c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")`,
+    height: '500px',
+    backgroundPosition: 'center',
+    bakcgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    position: 'relative',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: '#fff',
+    fontSize: '4rem'
+  }
+}));
 
 function App() {
+
+  const classes = useStyles()
+
   return (
     <div className="App">
-      <AppBar position="static">
+      <AppBar className={classes.appBar} position="static">
         <Toolbar>
-            <Typography variant="h6">
+            <Typography variant="h6" color="primary">
               Blog
             </Typography>
         </Toolbar>
       </AppBar>
+      <Box className={classes.hero}>
+        <Box>React Blog</Box>
+      </Box>
     </div>
   );
 }
